36a38956cf653090fe9f23a36425bd32515ced04
Paulo Sergio <pauloserg861@gmail.com>
2024-09-08 13:40:39 -0300

    feat: create main dashboard page

---
+++ src/components/ToolServices/index.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { Icon } from "@iconify/vue";
+import { useRouter } from "vue-router";
+import { Navigate } from "../../types/Navigate";
+import Typography from "../Typography/index.vue";
+import openAi from "../../assets/img/openai.jpg";
+
+defineProps<Navigate>();
+
+const redirect = useRouter();
+
+const toRouter = (path: string) => {
+  redirect.push(path);
+};
+</script>
+
+<template>
+  <div @click="toRouter(router)" class="card">
+    <header class="flex items-start justify-between">
+      <div class="bg-black p-2 rounded-full">
+        <Icon :icon="`tabler:${icon}`" class="text-white" />
+      </div>
+      <div class="w-7 ml-8">
+        <img :src="openAi" alt="openai"><!-- Repassar via props -->
+      </div>
+    </header>
+    <h3 class="font-semibold text-zinc-900 my-2">{{ description }}</h3>
+    <div>
+      <Typography tag="p" variant="body" class="text-gray-500">
+        {{ content }}
+      </Typography>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.card {
+  @apply shadow-md 
+    bg-white 
+    w-[30%] 
+    rounded-[18px] 
+    p-2 
+    h-[10.5rem] 
+    cursor-pointer 
+    border-2 
+    border-white 
+    hover:border-slate-300;
+}
+</style>
---
+++ src/views/app/MyApps/Dashboard.vue
@@ -0,0 +1,64 @@
+<script lang="ts" setup>
+//import { inject, ref, onMounted } from 'vue';
+import { Icon } from '@iconify/vue';
+import PageLayout from '../../../Layout/PageLayout.vue';
+import MainTitle from '../../../components/MainTitle/index.vue';
+import ToolServices from '../../../components/ToolServices/index.vue';
+//import { OpenaiService } from '../../../services/openai/OpenaiService';
+import { useNavigation } from '../../../composables/useNavigation';
+
+const { navigationServices } = useNavigation();
+
+// const openaiService = inject<OpenaiService>('openaiService');
+// const data = ref<typeof OpenaiService | null>(null)
+
+// const previousRouteName = ref('')
+
+function redirectTo() {
+  console.log('Redirecionando...');
+}
+</script>
+
+<template>
+  <PageLayout>
+    <MainTitle>Meus guIA Apps</MainTitle>
+
+    <div class="border my-4"></div>
+    <div>
+      <div class="flex">
+        <div class="flex items-center">
+          <Icon icon="iconamoon:arrow-bottom-left-6-circle-bold" class="mr-2" />
+          <Typography 
+            tag="p" 
+            variant="body" 
+            class="text-gray-900 font-semibold text-md"
+          >
+            Acessada recentemente:
+          </Typography>
+        </div>
+        <div class="ml-5 cursor-pointer flex items-center bg-white p-2 rounded-[18px] border-2 border-transparent hover:border-slate-300 shadow-sm">
+          <div class="bg-black p-2 flex items-center justify-center rounded-full mr-2">
+            <Icon icon="tdesign:share" class="text-white"/>
+          </div>
+          <span class="font-semibold" @click="redirectTo">
+            {{ 'previousRouteName' }}
+          </span>
+        </div>
+      </div>
+    </div>
+    <div class="border my-4"></div>
+
+    <div class="mt-4">
+      <Typography tag="p" variant="body" class="text-gray-500">
+        IA Workflows recomendados para o seu usuário
+      </Typography>
+      <div class="flex items-center gap-3 flex-wrap mb-8 mt-5">
+        <ToolServices
+          v-for="service in navigationServices"
+          :key="service.id"
+          v-bind="service"
+        />
+      </div>
+    </div>
+  </PageLayout>
+</template>
