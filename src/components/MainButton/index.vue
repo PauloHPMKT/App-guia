009ab3bb33ed4c60751768fccf762716f437ac55
Paulo Sergio <pauloserg861@gmail.com>
2024-10-14 20:50:03 -0300

    refactor: add propety to centralize button content

--- src/components/MainButton/index.vue
+++ src/components/MainButton/index.vue
@@ -4,7 +4,7 @@
 <template>
   <button 
     v-bind="$attrs"
-    class="bg-[#121212] text-white flex items-center gap-3 py-2 px-4 rounded-md"
+    class="bg-[#121212] text-white flex items-center justify-center gap-3 py-2 px-4 rounded-md"
   >
     <slot name="icon"></slot>
     <slot name="label"></slot>
